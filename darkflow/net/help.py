2525d3749ac483c9295c85703f0edb03e21b68c5
Anas Al Salah <anasx.al.salah@intel.com>
2018-04-30 12:17:22 -0700

    - added backward compatibility with opencv v2 - allowed opencv window creation to view video frames capture as well as camera

--- darkflow/net/help.py
+++ darkflow/net/help.py
@@ -4,7 +4,6 @@ tfnet secondary (helper) methods
 from ..utils.loader import create_loader
 from time import time as timer
 import tensorflow as tf
-import numpy as np
 import sys
 import cv2
 import os
@@ -26,7 +25,7 @@ def load_from_ckpt(self):
             load_point = load_point.split('"')[1]
             load_point = load_point.split('-')[-1]
             self.FLAGS.load = int(load_point)
-    
+
     load_point = os.path.join(self.FLAGS.backup, self.meta['name'])
     load_point = '{}-{}'.format(load_point, self.FLAGS.load)
     self.say('Loading from {}'.format(load_point))
@@ -41,10 +40,10 @@ def say(self, *msgs):
         if msg is None: continue
         print(msg)
 
-def load_old_graph(self, ckpt): 
+def load_old_graph(self, ckpt):
     ckpt_loader = create_loader(ckpt)
     self.say(old_graph_msg.format(ckpt))
-    
+
     for var in tf.global_variables():
         name = var.name.split(':')[0]
         args = [name, var.get_shape()]
@@ -57,56 +56,53 @@ def load_old_graph(self, ckpt):
         self.sess.run(op, {plh: val})
 
 def _get_fps(self, frame):
-    elapsed = int()
     start = timer()
     preprocessed = self.framework.preprocess(frame)
     feed_dict = {self.inp: [preprocessed]}
     net_out = self.sess.run(self.out, feed_dict)[0]
-    processed = self.framework.postprocess(net_out, frame, False)
+    self.framework.postprocess(net_out, frame, False)
     return timer() - start
 
 def camera(self):
     file = self.FLAGS.demo
     SaveVideo = self.FLAGS.saveVideo
-    
+
     if file == 'camera':
         file = 0
     else:
         assert os.path.isfile(file), \
         'file {} does not exist'.format(file)
-        
+
     camera = cv2.VideoCapture(file)
-    
-    if file == 0:
-        self.say('Press [ESC] to quit demo')
-        
+
+    self.say('Press [ESC] to quit demo')
+
     assert camera.isOpened(), \
     'Cannot capture source'
-    
-    if file == 0:#camera window
-        cv2.namedWindow('', 0)
-        _, frame = camera.read()
-        height, width, _ = frame.shape
-        cv2.resizeWindow('', width, height)
-    else:
-        _, frame = camera.read()
-        height, width, _ = frame.shape
+
+    cv2.namedWindow('', 0)
+    _, frame = camera.read()
+    height, width, _ = frame.shape
+    cv2.resizeWindow('', width, height)
 
     if SaveVideo:
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        if file == 0:#camera window
+        # check opencv version and configure videoWriter accordingly
+        print("Saving the video using opencv version: " + cv2.__version__)
+        cv_is_v2 = cv2.__version__.startswith("2")
+        fourcc = cv2.cv.CV_FOURCC(*'XVID') if cv_is_v2 else cv2.VideoWriter_fourcc(*'XVID')
+        if file == 0:  # camera window
           fps = 1 / self._get_fps(frame)
           if fps < 1:
             fps = 1
         else:
-            fps = round(camera.get(cv2.CAP_PROP_FPS))
+            fps = round(camera.get(cv2.cv.CV_CAP_PROP_FPS)) if cv_is_v2 else round(camera.get(cv2.CAP_PROP_FPS))
         videoWriter = cv2.VideoWriter(
             'video.avi', fourcc, fps, (width, height))
 
     # buffers for demo in batch
     buffer_inp = list()
     buffer_pre = list()
-    
+
     elapsed = int()
     start = timer()
     self.say('Press [ESC] to quit demo')
@@ -120,7 +116,7 @@ def camera(self):
         preprocessed = self.framework.preprocess(frame)
         buffer_inp.append(frame)
         buffer_pre.append(preprocessed)
-        
+
         # Only process and imshow when queue is full
         if elapsed % self.FLAGS.queue == 0:
             feed_dict = {self.inp: buffer_pre}
@@ -130,8 +126,8 @@ def camera(self):
                     single_out, img, False)
                 if SaveVideo:
                     videoWriter.write(postprocessed)
-                if file == 0: #camera window
-                    cv2.imshow('', postprocessed)
+                # draw image to opencv window
+                cv2.imshow('', postprocessed)
             # Clear Buffers
             buffer_inp = list()
             buffer_pre = list()
@@ -141,16 +137,16 @@ def camera(self):
             sys.stdout.write('{0:3.3f} FPS'.format(
                 elapsed / (timer() - start)))
             sys.stdout.flush()
-        if file == 0: #camera window
-            choice = cv2.waitKey(1)
-            if choice == 27: break
+
+        choice = cv2.waitKey(1)
+        if choice == 27: break
 
     sys.stdout.write('\n')
     if SaveVideo:
         videoWriter.release()
+
     camera.release()
-    if file == 0: #camera window
-        cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 def to_darknet(self):
     darknet_ckpt = self.darknet
